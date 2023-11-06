fa8423d8d624b2f9fab7327137fc2b07c3dc776b
Aizhan <aizhanchoiubekova@gmail.com>
2023-11-06 19:04:56 +0600

    added js

---
+++ index.js
@@ -0,0 +1,24 @@
+const current_year = 2023;
+const correct_answer = 'Ответ правильный';
+const error_answer = 'Ответ неправильный';
+
+
+const inputNode = document.querySelector('.js-input');
+const buttonNode = document.querySelector('.js-button');
+const outputNode = document.querySelector('.js-output');
+
+buttonNode.addEventListener('click', function(){
+    const inputValue = inputNode.value;
+  
+
+    if (!inputValue){
+        return
+    }
+    const answer = Number(inputNode.value);
+    let output = correct_answer;
+
+    if(answer !== current_year){
+        output = error_answer
+    }
+   outputNode.innerHTML= output;
+});
