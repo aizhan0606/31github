01a9224355cffd13aa81e524edec24b962174c64
Aizhan <aizhanchoiubekova@gmail.com>
2023-11-23 19:55:49 +0600

    added motion

--- index.js
+++ index.js
@@ -58,3 +58,64 @@ buttons.map((button) => {
     });
 }); */
 
+const cols = document.querySelectorAll('.col')
+
+document.addEventListener('keydown', (event) => {
+    if (event.code.toLocaleLowerCase() === 'space'){
+        setRandomColors()
+    }
+
+})
+
+document.addEventListener('click', (event) => {
+    const type = event.target.dataset.type
+
+    if (type === 'lock'){
+        const node = 
+        event.target.tagName.toLowerCase() === 'i'
+        ? event.target
+        : event.target.children[0]
+
+
+        node.classList.toggle('fa-lock-open') 
+        node.classList.toggle('fa-lock')
+    }
+})
+
+function gerenerateRandomColor() {
+    //RGB
+    //#FF0000
+    //#00FF00
+    //#0000FF
+
+    const hexCodes = '0123456789ABCDEF'
+
+    let color = ''
+    for (let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+    }
+    return '#' + color
+}
+
+
+
+function setRandomColors() {
+    cols.forEach((col) =>{
+        const text = col.querySelector('h2')
+        const button = col.querySelector('button')
+        const color = chroma.random()
+
+        text.textContent = color
+        col.style.background = color
+
+        setTextColor(text, color)
+        setTextColor(button, color)
+    })
+}
+
+function setTextColor(text, color){
+   const luminance = chroma(color).luminance()
+   text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
+
+setRandomColors()
