9749cdcb5c3d5ae97e078a98d7a5bd1510df8f66
Aizhan <aizhanchoiubekova@gmail.com>
2023-11-28 21:04:55 +0600

    added if else

--- leetcode.7.js
+++ leetcode.7.js
@@ -3,7 +3,21 @@
 const input = [5, 1, 2, 1, 2];
 
 const singleNumber1 = function(nums){
-    
+   let uniq = new Set();
+   let uniqSum = 0;
+   let numSum = 0;
+
+   for (let i = 0; i < nums.length; i++){
+    const current = nums[i];
+
+    if(!uniq.has(current)){
+        uniq.add(current);
+        uniqSum += current;
+    }
+    numSum += current;
+   }
+   return uniqSum * 2 - numSum;
+
 };
 
 console.log(singleNumber1(input));
